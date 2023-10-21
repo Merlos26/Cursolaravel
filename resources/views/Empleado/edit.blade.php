0ebdd5c9b68b28a265547db29067d1a0f1192f02
Merlos26 <rm12100@ues.edu.sv>
2023-10-20 23:06:54 -0600

    primeraversion2

--- resources/views/Empleado/edit.blade.php
+++ resources/views/Empleado/edit.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<h1>Nuevo cambio</h1>
 <div class="container">
 
 <form action="{{url('/Empleado/'.$empleado->id)}}" method="post" enctype="multipart/form-data">
